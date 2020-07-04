15de7da51b1a37310799f8ab8a8533bb8e8add7c
Loi Van Tran <datloiboi@gmail.com>
2020-07-04 16:06:46 +0700

    Oops, remove key

--- utils/geocode.js
+++ utils/geocode.js
@@ -29,7 +29,7 @@ const forecast = (lat, long, callback) => {
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
-  )}.json?access_token=pk.eyJ1IjoicHJpbWV0aW1ldHJhbiIsImEiOiJjano0N3hnNmQwNTZ1M2VwbWt6eTZ2cjh1In0.qPgFE-kkU3DnxOWbRsd7gw`;
+  )}.json?access_token=${process.env.MAPBOX_API_KEY}`;
   request({ url, json: true }, (error, response) => {
     if (error) {
       callback("Unable to connect to location services.");
